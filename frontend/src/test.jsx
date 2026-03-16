d627d4af0ea63709477ca75646a06bace27b2cdc
reiner-u <rethan.umila@gmail.com>
2026-03-16 13:25:57 -0400

    Added ability to input different cities to view current available weather data
    feat: added user input finally
    fix: fixed bug where Nomatim was overloaded due to {city} updating upon keystroke rather than when search button was pressed

--- frontend/src/test.jsx
+++ frontend/src/test.jsx
@@ -6,8 +6,19 @@ function FetchData() {
     const [data, setData] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
-    const [city, setCity] = useState("Toronto"); // hardcoded city for testing, setCity for future user input
+    const [city, setCity] = useState("Manila"); // hardcoded city for testing, setCity for future user input
+    const [userInput, setUserInput] = useState("");
+    const handleCityChange = (event) => {
+        setUserInput(event.target.value);
+    };
 
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        setIsLoading(true);
+        setError(null);
+        setCity(userInput) //had issues with requests overloading due to useEffect running on every keystroke, so I moved setCity here to only update city when form is submitted, which will trigger useEffect to run fetchData again with new city
+        //fetchData will be called in useEffect, which will run again when city state changes
+    }
     
 useEffect(() => {
     const fetchData = async () => {
@@ -28,7 +39,7 @@ useEffect(() => {
     };
 
     fetchData();
-}, []);
+}, [city]); // dependency array includes city, so fetchData will run again when city changes
 
 function renderContent() {
     if (isLoading) {
@@ -37,8 +48,19 @@ function renderContent() {
     if (error) {
         return <p>Error: {error}</p>;
     }
+
+    
     return (
         <div>
+            <form onSubmit={handleFormSubmit}>
+                <label>
+                    Enter City to view weather data:
+                    <input type="text" value={userInput} onChange={handleCityChange}
+                    placeholder="Enter city name"/>
+                </label>
+                <button type="submit" disabled={isLoading}>Get Weather</button>{/*button is disabled while loading to prevent multiple requests*/}
+            </form>
+
             <h1>{city} Weather Data</h1>
             {data && (
                 <div>
